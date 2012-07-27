0f29a66d1fc098a9e9691a9ed4f35d4d44c38da7
Ubuntu 10.10 <csharma@ubuntu.(none)>
2012-07-27 08:22:06 -0400

    - refactored price-trends commandline program.

--- pricetrends_cmd.py
+++ pricetrends_cmd.py
@@ -3,12 +3,10 @@ import sys
 
 from settings import *
 
-
 connection = mdb.connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 base_query = "select *, count(*), sum(price)/count(*) AS 'avg_price' from pricelist \
          where %s = '%s' AND listed_date >= '%s' AND listed_date <= '%s' \
          group by listed_date order by listed_date asc"
-
 
 def fetchTrendsByQuery(query):
     """ fetch trends by query. """
@@ -16,49 +14,34 @@ def fetchTrendsByQuery(query):
     cur.execute(query)
     return cur.fetchall()    
 
-
 def productTrendsById(productId, startDt, endDt):
     """ fetch product trends for the startDt and endDt. """
-
     query = base_query % ('productId', productId, startDt, endDt)
     return fetchTrendsByQuery(query)
 
-
 def categoryTrendsById(productId, startDt, endDt):
     """ fetch category trends for the startDt and endDt. """
-
-    query = "select *, count(*), sum(price)/count(*) AS 'avg_price' from pricelist \
-             where categoryId = '%s' AND listed_date >= '%s' AND listed_date <= '%s' \
-             group by listed_date order by listed_date asc"  % (productId, startDt, endDt)
-
+    query = base_query % ('categoryId', productId, startDt, endDt)
     return fetchTrendsByQuery(query)
-
 
 def brandTrendsByName(productId, startDt, endDt):
     """ fetch category trends for the startDt and endDt. """
-
-    query = "select *, count(*), sum(price)/count(*) AS 'avg_price' from pricelist \
-             where brand = '%s' AND listed_date >= '%s' AND listed_date <= '%s' \
-             group by listed_date order by listed_date asc"  % (productId, startDt, endDt)
-
+    query = base_query % ('brand', productId, startDt, endDt)
     return fetchTrendsByQuery(query)
-
 
 def printTrends(trends):
     """ print trends in human readable format. """
     for trend in trends:
         print trend
-
+    print
 
 if __name__ == '__main__':
     trends = productTrendsById('101332', '2012-02-06', '2012-02-29')
     printTrends(trends)
 
-    # print
-    # trends = categoryTrendsById('1', '2012-02-06', '2012-02-29')
-    # printTrends(trends)
+    trends = categoryTrendsById('1', '2012-02-06', '2012-02-29')
+    printTrends(trends)
     
-    # print
-    # trends = brandTrendsByName('Alcatel', '2012-02-06', '2012-02-29')
-    # printTrends(trends)
+    trends = brandTrendsByName('Sony', '2012-02-06', '2012-02-29')
+    printTrends(trends)
     
